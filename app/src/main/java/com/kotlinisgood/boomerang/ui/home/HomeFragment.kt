08d35fdc88d297beac37880c9a2dbde425de7c71
greenddoovie <mania01234@naver.com>
2021-11-02 14:36:30 +0900

    feat: Home Fragment Toolbar 버튼, 타이틀 UI 구현

--- app/src/main/java/com/kotlinisgood/boomerang/ui/home/HomeFragment.kt
+++ app/src/main/java/com/kotlinisgood/boomerang/ui/home/HomeFragment.kt
@@ -1,10 +1,10 @@
 package com.kotlinisgood.boomerang.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.kotlinisgood.boomerang.R
 
 class HomeFragment : Fragment() {
@@ -14,5 +14,10 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
     }
 }
