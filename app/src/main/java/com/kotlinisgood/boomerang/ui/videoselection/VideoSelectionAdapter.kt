fd5244daab84c2fd09185338ea999d3c31e799d7
greenddoovie <mania01234@naver.com>
2021-11-03 15:36:16 +0900

    feat: VideoSelection 체크박스 기능 추가

--- app/src/main/java/com/kotlinisgood/boomerang/ui/videoselection/VideoSelectionAdapter.kt
+++ app/src/main/java/com/kotlinisgood/boomerang/ui/videoselection/VideoSelectionAdapter.kt
@@ -15,21 +15,41 @@ import com.kotlinisgood.boomerang.ui.home.ExternalVideoDiffItemCallback
 class VideoSelectionAdapter(
     private val contentResolver: ContentResolver
 ) : ListAdapter<ExternalVideoDTO, VideoSelectionAdapter.VideoSelectionViewHolder>(
-        ExternalVideoDiffItemCallback
-    ) {
+    ExternalVideoDiffItemCallback
+) {
+
+    var selectedIndex = -1
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoSelectionViewHolder {
-        return VideoSelectionViewHolder.from(parent, contentResolver)
+        return VideoSelectionViewHolder(
+            ItemRvVideoSelectionShowVideosBinding
+                .inflate(LayoutInflater.from(parent.context), parent, false),
+            contentResolver
+        )
     }
 
     override fun onBindViewHolder(holder: VideoSelectionViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
 
-    class VideoSelectionViewHolder(
+    inner class VideoSelectionViewHolder(
         private val binding: ItemRvVideoSelectionShowVideosBinding,
         private val contentResolver: ContentResolver
     ) : RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.root.setOnClickListener {
+                if (selectedIndex != -1) {
+                    currentList[selectedIndex].isChecked = false
+                }
+                notifyItemChanged(selectedIndex)
+                adapterPosition.also {
+                    currentList[it].isChecked = true
+                    selectedIndex = it
+                    notifyItemChanged(it)
+                }
+            }
+        }
 
         fun bind(item: ExternalVideoDTO) {
             val thumbnail = MediaMetadataRetriever().run {
@@ -38,29 +58,17 @@ class VideoSelectionAdapter(
                 getFrameAtTime(0L)
             }
             binding.itemIvVideoSelectionThumbnail.setImageBitmap(thumbnail)
+            binding.itemCbVideoSelection.isChecked = item.isChecked
         }
 
         @SuppressLint("Range")
-        fun getPathFromUri(uri: Uri): String{
-            val cursor = contentResolver.query(uri, null, null, null, null)?: return ""
+        fun getPathFromUri(uri: Uri): String {
+            val cursor = contentResolver.query(uri, null, null, null, null) ?: return ""
             cursor.moveToNext()
             val path = cursor.getString(cursor.getColumnIndex("_data"))
             cursor.close()
             return path
         }
-
-        companion object {
-            fun from(parent: ViewGroup, contentResolver: ContentResolver): VideoSelectionViewHolder {
-                return VideoSelectionViewHolder(
-                    ItemRvVideoSelectionShowVideosBinding
-                        .inflate(
-                            LayoutInflater.from(parent.context),
-                            parent,
-                            false
-                        ),
-                    contentResolver
-                )
-            }
-        }
     }
+
 }
