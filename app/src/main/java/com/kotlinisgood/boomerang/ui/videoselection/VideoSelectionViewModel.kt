4741faf37b91ebb74a5fc61298f92243844111b6
uzucode <uzucode@gmail.com>
2021-11-03 19:08:13 +0900

    chore: 불필요한 home에 있는 data를 videoselection package로 옮김

--- app/src/main/java/com/kotlinisgood/boomerang/ui/videoselection/VideoSelectionViewModel.kt
+++ app/src/main/java/com/kotlinisgood/boomerang/ui/videoselection/VideoSelectionViewModel.kt
@@ -3,8 +3,6 @@ package com.kotlinisgood.boomerang.ui.videoselection
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.kotlinisgood.boomerang.ui.home.ExternalVideoDTO
-import com.kotlinisgood.boomerang.ui.home.VideoGallery
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
