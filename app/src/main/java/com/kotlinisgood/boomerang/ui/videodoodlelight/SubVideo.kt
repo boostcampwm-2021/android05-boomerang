a06e86a0e9bacd7200bb6c85ba83566e67f5796e
jinhyungchoi <wlsgudchoi@gmail.com>
2021-11-08 18:44:33 +0900

    fix: Room 저장 위해 uri String 으로 변경

--- app/src/main/java/com/kotlinisgood/boomerang/ui/videodoodlelight/SubVideo.kt
+++ app/src/main/java/com/kotlinisgood/boomerang/ui/videodoodlelight/SubVideo.kt
@@ -1,8 +1,7 @@
 package com.kotlinisgood.boomerang.ui.videodoodlelight
 
-import android.net.Uri
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class SubVideo(var uri: Uri, val startingTime: Int, var endingTime: Int) : Parcelable
+data class SubVideo(var uri: String, val startingTime: Int, var endingTime: Int) : Parcelable
