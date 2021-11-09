ece82936aeccb704860932aa4751bcc9aacc1e29
jinhyungchoi <wlsgudchoi@gmail.com>
2021-11-09 18:46:23 +0900

    feat: 동영상 재생 관련 Issue 해결 #36, #37, #39, #41

--- app/src/main/java/com/kotlinisgood/boomerang/ui/videoedit/VideoEditFragment.kt
+++ app/src/main/java/com/kotlinisgood/boomerang/ui/videoedit/VideoEditFragment.kt
@@ -1,9 +1,12 @@
 package com.kotlinisgood.boomerang.ui.videoedit
 
+import android.media.MediaPlayer
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.core.net.toUri
 import androidx.core.widget.doOnTextChanged
 import androidx.databinding.DataBindingUtil
@@ -12,11 +15,13 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.alphamovie.lib.AlphaMovieView
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.kotlinisgood.boomerang.R
 import com.kotlinisgood.boomerang.databinding.FragmentVideoEditBinding
+import com.kotlinisgood.boomerang.ui.videodoodlelight.SubVideo
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.*
 
@@ -27,18 +32,14 @@ class VideoEditFragment : Fragment() {
     private val viewModel: VideoEditViewModel by viewModels()
     private val args: VideoEditFragmentArgs by navArgs()
 
-    private var isPlayings = mutableListOf<Boolean>()
-
-    private lateinit var player: SimpleExoPlayer
-
-    lateinit var jobScanner: Job
-    lateinit var jobTimer: Job
-
-    @Volatile
-    private var isPlaying = false
-
     @Volatile
     private var currentTime = 0L
+    private lateinit var player: SimpleExoPlayer
+    lateinit var jobScanner: Job
+
+    private lateinit var alphaViewFactory: AlphaViewFactory
+    private var currentAlpha: AlphaMovieView? = null
+    private var currentSubVideo: SubVideo? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -55,16 +56,25 @@ class VideoEditFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setAlphaViewFactory()
         setViewModel()
         setVideoView()
-        setScanner()
         setPlayer()
         setListener()
+    }
+
+    private fun setAlphaViewFactory() {
+        alphaViewFactory = AlphaViewFactory(requireContext())
     }
 
     private fun setViewModel() {
         viewModel.setSubVideo(args.subVideos.toMutableList())
         viewModel.setVideoUri(args.baseVideo.toUri())
+        viewModel.getSubVideo().forEach {
+            val alphaView = alphaViewFactory.create()
+            alphaView.setVideoFromUri(requireContext(), it.uri.toUri())
+            viewModel.addAlphaMovieView(alphaView)
+        }
     }
 
     private fun setListener() {
@@ -79,10 +89,11 @@ class VideoEditFragment : Fragment() {
     }
 
     private fun setVideoView() {
-        player = SimpleExoPlayer.Builder(requireContext()).build()
-        binding.exoplayer.player = player
         val mediaItem = MediaItem.fromUri(viewModel.getVideoUri())
+        player = SimpleExoPlayer.Builder(requireContext()).build()
         player.setMediaItem(mediaItem)
+        binding.exoplayer.player = player
+
         var string = ""
         viewModel.getSubVideo().forEach {
             string += "$it\n"
@@ -94,74 +105,97 @@ class VideoEditFragment : Fragment() {
         player.addListener(onPlayStateChangeListener)
     }
 
-    private suspend fun timer() {
-        withContext(Dispatchers.Default) {
-            while (true) {
-                if (isPlaying) {
-                    withContext(Dispatchers.Main) {
-                        currentTime = player.currentPosition
-                    }
-                }
-                delay(500)
-            }
-        }
-    }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private suspend fun scan() {
         withContext(Dispatchers.Default) {
             while (true) {
-                if (isPlaying) {
-                    viewModel.getSubVideo().forEachIndexed { index, subVideo ->
-                        println("$index")
-                        val time = currentTime
-                        // 실행시켜야되는지?
-                        if ((subVideo.startingTime / 1000).toLong() <= time / 1000 &&
-                            (subVideo.endingTime / 1000).toLong() >= time / 1000 &&
-                            !isPlayings[index]
-                        ) {
+                viewModel.getSubVideo().forEachIndexed { index, subVideo ->
+                    withContext(Dispatchers.Main) {
+                        currentTime = player.currentPosition
+                    }
+                    if ((subVideo.startingTime).toLong() <= currentTime &&
+                        (subVideo.endingTime).toLong() >= currentTime
+                    ) {
+                        if (!viewModel.getSubVideoStates()[index]) {
                             withContext(Dispatchers.Main) {
-                                isPlayings[index] = true
-                                binding.alphaView.setVideoFromUri(context, subVideo.uri.toUri())
-                                binding.alphaView.mediaPlayer.setOnPreparedListener {
-                                    binding.alphaView.mediaPlayer.start()
-                                    binding.alphaView.setLooping(false)
-                                    binding.alphaView.seekTo((time - subVideo.startingTime.toLong()).toInt())
-                                    binding.alphaView.visibility = View.VISIBLE
-                                }
-                                binding.alphaView.setOnVideoEndedListener {
-                                    isPlayings[index] = false
-                                    binding.alphaView.visibility = View.GONE
-                                    binding.alphaView.seekTo(0)
+                                // 현재 재생 중이던 영상과 subVideo 가 같을 때
+                                if (subVideo == currentSubVideo) {
+                                    viewModel.getSubVideoStates()[index] = true
+                                    currentAlpha!!.mediaPlayer.seekTo(
+                                        (player.currentPosition - subVideo.startingTime.toLong()),
+                                        MediaPlayer.SEEK_CLOSEST
+                                    )
+                                    currentAlpha!!.mediaPlayer.start()
+                                } else {
+                                    binding.alphaView.removeAllViews()
+                                    // 현재 재생 중이던 AlphaView 가 있을 시
+                                    if (currentSubVideo != null) {
+                                        resetAlphaView(currentSubVideo!!)
+                                        currentSubVideo = null
+                                        currentAlpha = null
+                                    }
+
+                                    val alphaMovieView = viewModel.alphaMovieViews[index]
+                                    viewModel.getSubVideoStates()[index] = true
+                                    currentSubVideo = subVideo
+                                    currentAlpha = alphaMovieView
+
+                                    binding.alphaView.addView(alphaMovieView)
+                                    alphaMovieView.start()
+                                    alphaMovieView.setOnVideoStartedListener {
+                                        alphaMovieView.seekTo((currentTime - subVideo.startingTime.toLong()).toInt())
+                                    }
+                                    alphaMovieView.setOnVideoEndedListener {
+                                        binding.alphaView.removeAllViews()
+                                        resetAlphaView(currentSubVideo!!)
+                                        currentSubVideo = null
+                                        currentAlpha = null
+                                    }
                                 }
                             }
                         }
                     }
+                    if(!viewModel.getSubVideoStates().contains(true)) {
+                        withContext(Dispatchers.Main) {
+                            if (currentSubVideo != null) {
+                                binding.alphaView.removeAllViews()
+                                resetAlphaView(currentSubVideo!!)
+                                currentSubVideo = null
+                                currentAlpha = null
+                            }
+                        }
+                    }
                 }
-                delay(500)
+                delay(10)
             }
         }
-    }
-
-    private fun setScanner() {
-        viewModel.getSubVideo().size.let { repeat(it) { isPlayings.add(false) } }
     }
 
     private val onPlayStateChangeListener = object : Player.Listener {
+        @RequiresApi(Build.VERSION_CODES.O)
         override fun onIsPlayingChanged(isPlay: Boolean) {
-            isPlaying = isPlay
             if (isPlay) {
                 jobScanner = lifecycleScope.launch { scan() }
-                jobTimer = lifecycleScope.launch { timer() }
-                jobScanner.start()
-                jobTimer.start()
             } else {
                 jobScanner.cancel()
-                jobTimer.cancel()
-                binding.alphaView.mediaPlayer.reset()
-                isPlayings.forEachIndexed { index, _ -> isPlayings[index] = false }
-                binding.alphaView.visibility = View.GONE
+                if (currentAlpha != null && currentAlpha!!.isPlaying) {
+                    currentAlpha!!.mediaPlayer.pause()
+                }
+                viewModel.getSubVideoStates()
+                    .forEachIndexed { index, _ -> viewModel.getSubVideoStates()[index] = false }
             }
         }
+    }
+
+    private fun resetAlphaView(currentSubVideo: SubVideo) {
+        val alphaView = alphaViewFactory.create()
+        alphaView.setVideoFromUri(
+            requireContext(),
+            currentSubVideo.uri.toUri()
+        )
+        val index = viewModel.getSubVideo().indexOf(currentSubVideo)
+        viewModel.alphaMovieViews[index] = alphaView
     }
 
     companion object {
