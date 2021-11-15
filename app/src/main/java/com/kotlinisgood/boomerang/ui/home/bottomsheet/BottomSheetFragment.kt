d0a3370f00a6b1c2c31788b1337d8fe461b1e4b2
jinhyungchoi <wlsgudchoi@gmail.com>
2021-11-15 21:03:54 +0900

    feat: BottomSheetFragment 추가 및 기능 구현

---
+++ app/src/main/java/com/kotlinisgood/boomerang/ui/home/bottomsheet/BottomSheetFragment.kt
@@ -0,0 +1,75 @@
+package com.kotlinisgood.boomerang.ui.home.bottomsheet
+
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.kotlinisgood.boomerang.R
+import com.kotlinisgood.boomerang.databinding.FragmentBottomSheetBinding
+import com.kotlinisgood.boomerang.model.OrderState
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class BottomSheetFragment : BottomSheetDialogFragment(){
+
+    private lateinit var binding : FragmentBottomSheetBinding
+    private val viewModel: BottomSheetFragmentViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentBottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setViewModel()
+        setListener()
+        setObserver()
+    }
+
+    private fun setObserver() {
+        viewModel.orderSetting.observe(viewLifecycleOwner){
+            viewModel.setCheckBox()
+        }
+    }
+
+    private fun setViewModel() {
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+    }
+
+    private fun setListener(){
+        binding.tvCreateOld.setOnClickListener {
+            if(viewModel.orderSetting.value != OrderState.CREATE_OLD){
+                viewModel.setOrderState(OrderState.CREATE_OLD)
+                findNavController().navigate(R.id.action_bottomSheetFragment_to_homeFragment)
+            }
+        }
+        binding.tvCreateRecent.setOnClickListener {
+            if(viewModel.orderSetting.value != OrderState.CREATE_RECENT){
+                viewModel.setOrderState(OrderState.CREATE_RECENT)
+                findNavController().navigate(R.id.action_bottomSheetFragment_to_homeFragment)
+            }
+        }
+        binding.tvEditRecent.setOnClickListener {
+            if(viewModel.orderSetting.value != OrderState.MODIFY_RECENT){
+                viewModel.setOrderState(OrderState.MODIFY_RECENT)
+                findNavController().navigate(R.id.action_bottomSheetFragment_to_homeFragment)
+            }
+        }
+        binding.tvEditOld.setOnClickListener {
+            if(viewModel.orderSetting.value != OrderState.MODIFY_OLD){
+                viewModel.setOrderState(OrderState.MODIFY_OLD)
+                findNavController().navigate(R.id.action_bottomSheetFragment_to_homeFragment)
+            }
+        }
+    }
+}
