736033e9205398bf01ef3b21bf29ef49f807ddd9
jinhyungchoi <wlsgudchoi@gmail.com>
2021-11-15 14:57:17 +0900

    feat: SubVideoAdapter OnSubVideoClickListener 추가

--- app/src/main/java/com/kotlinisgood/boomerang/ui/videodoodlelight/SubVideoAdapter.kt
+++ app/src/main/java/com/kotlinisgood/boomerang/ui/videodoodlelight/SubVideoAdapter.kt
@@ -1,6 +1,7 @@
 package com.kotlinisgood.boomerang.ui.videodoodlelight
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -10,38 +11,42 @@ import com.kotlinisgood.boomerang.ui.videodoodlelight.SubVideoAdapter.SubVideoVi
 
 class SubVideoAdapter : ListAdapter<SubVideo, SubVideoViewHolder>(SubVideoDiffCallback()) {
 
+    var subVideoClickListener : OnSubVideoClickListener? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SubVideoViewHolder {
-        return SubVideoViewHolder.from(parent)
+        return SubVideoViewHolder(
+            ItemRvDoodleLightSubvideosBinding.inflate(
+                LayoutInflater.from(
+                    parent.context
+                ), parent, false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: SubVideoViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
 
-    class SubVideoViewHolder(
+    inner class SubVideoViewHolder(
         private val binding: ItemRvDoodleLightSubvideosBinding
     ) : RecyclerView.ViewHolder(binding.root) {
+        init{
+            itemView.setOnClickListener {
+                subVideoClickListener?.onItemClick(itemView,adapterPosition)
+            }
+        }
         fun bind(item: SubVideo) {
             binding.subVideo = item
         }
-
-        companion object {
-            fun from(parent: ViewGroup): SubVideoViewHolder {
-                return SubVideoViewHolder(
-                    ItemRvDoodleLightSubvideosBinding.inflate(
-                        LayoutInflater.from(
-                            parent.context
-                        ), parent, false
-                    )
-                )
-            }
-        }
     }
 
-    override fun submitList(list: MutableList<SubVideo>?) {
-        super.submitList(list)
+    interface OnSubVideoClickListener {
+        fun onItemClick(v: View, position: Int)
     }
 
+    fun setOnItemClickListener(listener: OnSubVideoClickListener){
+        subVideoClickListener = listener
+    }
 }
 
 class SubVideoDiffCallback : DiffUtil.ItemCallback<SubVideo>() {
