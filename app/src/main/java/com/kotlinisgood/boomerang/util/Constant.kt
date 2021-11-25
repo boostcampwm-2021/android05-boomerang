883fc103108b6f7740b1f83d92d036f5e897d9b2
jinhyungchoi <wlsgudchoi@gmail.com>
2021-11-25 23:14:05 +0900

    feat: 앱 인트로 구현

--- app/src/main/java/com/kotlinisgood/boomerang/util/Constant.kt
+++ app/src/main/java/com/kotlinisgood/boomerang/util/Constant.kt
@@ -14,6 +14,7 @@ const val VIDEO_MODE_SUB_VIDEO = 10000001
 const val AUDIO_MODE = 10000002
 const val PREF_NAME = "SETTING"
 const val ORDER_STATE = "orderState"
+const val IS_FIRST = "isFirst"
 
 
 fun View.throttle(duration: Long, timeUnit: TimeUnit, method: () -> Unit): Disposable {
