6656dbc946058ef0db72ace8d9d01bcf506f228c
jinhyungchoi <wlsgudchoi@gmail.com>
2021-11-29 11:03:32 +0900

    refactor: DrawView 코틀린으로 전환

---
+++ app/src/main/java/com/kotlinisgood/boomerang/ui/videodoodlelight/DrawView.kt
@@ -0,0 +1,92 @@
+package com.kotlinisgood.boomerang.ui.videodoodlelight
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.view.MotionEvent
+import android.view.View
+
+class DrawView(context: Context?) : View(context) {
+    private lateinit var bitmap: Bitmap
+    private lateinit var canvas: Canvas
+    private var path: Path = Path()
+    private var bitmapPaint: Paint = Paint(Paint.DITHER_FLAG)
+    private var paint: Paint = Paint()
+
+    private var mX = 0f
+    private  var mY:Float = 0f
+    private val TOUCH_TOLERANCE = 4f
+
+    init {
+        paint.color = -0x10000
+        paint.style = Paint.Style.STROKE
+        paint.strokeJoin = Paint.Join.ROUND
+        paint.strokeCap = Paint.Cap.ROUND
+        paint.strokeWidth = 10.0f
+        setBackgroundColor(0x00000000)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(bitmap)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(bitmap, 0f, 0f, bitmapPaint)
+        canvas.drawPath(path, paint)
+    }
+
+
+    private fun touch_start(x: Float, y: Float) {
+        path.reset()
+        path.moveTo(x, y)
+        mX = x
+        mY = y
+    }
+
+    private fun touch_move(x: Float, y: Float) {
+        val dx = Math.abs(x - mX)
+        val dy: Float = Math.abs(y - mY)
+        if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+            path.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2)
+            mX = x
+            mY = y
+        }
+    }
+
+    private fun touch_up() {
+        path.lineTo(mX, mY)
+        // commit the path to our offscreen
+        canvas.drawPath(path, paint)
+        // kill this so we don't double draw
+        path.reset()
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val x = event.x
+        val y = event.y
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                touch_start(x, y)
+                invalidate()
+            }
+            MotionEvent.ACTION_MOVE -> {
+                touch_move(x, y)
+                invalidate()
+            }
+            MotionEvent.ACTION_UP -> {
+                touch_up()
+                invalidate()
+            }
+        }
+        return true
+    }
+
+    fun setColor(color: Int) {
+        paint.setColor(color)
+    }
+}
