0c320a1b46cb8757df710dc27a02f7c956686de5
greenddoovie <mania01234@naver.com>
2021-11-29 16:25:24 +0900

    refactor: AudioRecordFragment 코드 리팩토링 진행

--- app/src/main/java/com/kotlinisgood/boomerang/ui/audiorecord/AudioRecordFragment.kt
+++ app/src/main/java/com/kotlinisgood/boomerang/ui/audiorecord/AudioRecordFragment.kt
@@ -4,12 +4,10 @@ import android.Manifest
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.media.MediaMetadataRetriever
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
 import android.speech.RecognizerIntent
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -41,25 +39,27 @@ import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class AudioRecordFragment : Fragment() {
-    private val TAG = "AudioRecordSecond"
-    private val permissionRejected = "Permission Not Granted By the User"
-    private val titleWarning = "타이틀을 입력해주세요"
-    private val audioListWarning = "인식된 음성이 존재하지 않습니다"
-    private val STTWarning = "구글앱 사용을 활성화해주시거나 구글 앱의 데이터를 삭제한 후 다시 시도해주세요. \n이동하시겠습니까?"
+
     private val loadingDialog by lazy { CustomLoadingDialog(requireContext()) }
+
     private val compositeDisposable by lazy { CompositeDisposable() }
+
     private var _dataBinding: FragmentAudioRecordBinding? = null
     val dataBinding get() = _dataBinding!!
 
     private val recognizerIntent by lazy { Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH) }
 
-    private val viewModel: AudioRecordViewModel by viewModels()
+    private val audioRecordViewModel: AudioRecordViewModel by viewModels()
 
+    private val permissions = arrayOf(
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.RECORD_AUDIO,
+    )
     private val permissionsResultCallback = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()
     ) {
         if (it.values.contains(false)) {
-            Toast.makeText(requireContext(), permissionRejected, Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), getString(R.string.permission_rejected), Toast.LENGTH_SHORT).show()
         } else {
             startSTT()
         }
@@ -99,28 +99,32 @@ class AudioRecordFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _dataBinding?.apply {
-            viewModel = viewModel
-            lifecycleOwner = viewLifecycleOwner
-        }
+        setBinding()
         setTbSetting()
-        setRecognizerListener()
+        setRecognizerIntentAttr()
         setObserver()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _dataBinding = null
-        if (!viewModel.isFileListEmpty()) { viewModel.deleteAudios() }
+        if (!audioRecordViewModel.isFileListEmpty()) { audioRecordViewModel.deleteAudios() }
         compositeDisposable.dispose()
     }
 
+    private fun setBinding() {
+        _dataBinding?.apply {
+            viewModel = audioRecordViewModel
+            lifecycleOwner = viewLifecycleOwner
+        }
+    }
+
     private fun setObserver() {
-        viewModel.audioMemo.observe(viewLifecycleOwner) {
+        audioRecordViewModel.audioMemo.observe(viewLifecycleOwner) {
             val action = AudioRecordFragmentDirections.actionAudioRecordFragmentToHomeFragment()
             findNavController().navigate(action)
         }
-        viewModel.loading.observe(viewLifecycleOwner) { loading ->
+        audioRecordViewModel.loading.observe(viewLifecycleOwner) { loading ->
             if (loading) {
                 loadingDialog.show()
             } else {
@@ -129,7 +133,7 @@ class AudioRecordFragment : Fragment() {
         }
     }
 
-    private fun setRecognizerListener() {
+    private fun setRecognizerIntentAttr() {
         recognizerIntent.apply {
             putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR")
             putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, requireActivity().packageName)
@@ -156,25 +160,8 @@ class AudioRecordFragment : Fragment() {
         }
     }
 
-    private fun checkAudioAndSave() {
-        if(dataBinding.etAudioRecordEnterTitle.text.toString() == "") {
-            Toast.makeText(requireContext(), titleWarning, Toast.LENGTH_SHORT).show()
-        } else if (viewModel.isFileListEmpty()) {
-            Toast.makeText(requireContext(), audioListWarning, Toast.LENGTH_SHORT).show()
-        } else {
-            val title = dataBinding.etAudioRecordEnterTitle.text.toString()
-            loadingDialog.show()
-            viewModel.saveAudioMemo(title, requireActivity().filesDir)
-        }
-    }
-
     private fun checkPermissions() {
-        val permissions = arrayOf(
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.RECORD_AUDIO,
-        )
         val rejected = mutableListOf<String>()
-
         permissions.forEach {
             if (ContextCompat.checkSelfPermission(
                     requireContext(),
@@ -197,25 +184,41 @@ class AudioRecordFragment : Fragment() {
         try {
             activityCallback.launch(recognizerIntent)
         } catch (e: ActivityNotFoundException) {
-                MaterialAlertDialogBuilder(requireContext())
-                    .setTitle("구글 앱에 문제가 있습니다.")
-                    .setMessage(STTWarning)
-                    .setNegativeButton("닫기") { dialog, which ->
-                        dialog.dismiss()
-                    }
-                    .setPositiveButton("이동") { dialog, which ->
-                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
-                            data = Uri.fromParts("package", "com.google.android.googlequicksearchbox",null)
-                        }
-                        startActivity(intent)
-                        dialog.dismiss()
-                    }
-                    .show()
+            e.printStackTrace()
+            makeGoogleErrorDialog()
         }
     }
 
+    private fun checkAudioAndSave() {
+        if(dataBinding.etAudioRecordEnterTitle.text.toString() == "") {
+            Toast.makeText(requireContext(), getString(R.string.title_warning), Toast.LENGTH_SHORT).show()
+        } else if (audioRecordViewModel.isFileListEmpty()) {
+            Toast.makeText(requireContext(), getString(R.string.audio_list_warning), Toast.LENGTH_SHORT).show()
+        } else {
+            val title = dataBinding.etAudioRecordEnterTitle.text.toString()
+            loadingDialog.show()
+            audioRecordViewModel.saveAudioMemo(title, requireActivity().filesDir)
+        }
+    }
+
+    private fun makeGoogleErrorDialog() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(getString(R.string.google_warning_title))
+            .setMessage(getString(R.string.stt_warning))
+            .setNegativeButton(getString(R.string.dialog_negative_close)) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .setPositiveButton(getString(R.string.dialog_positive_move)) { dialog, _ ->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                    data = Uri.fromParts("package", "com.google.android.googlequicksearchbox",null)
+                }
+                startActivity(intent)
+                dialog.dismiss()
+            }
+            .show()
+    }
+
     private suspend fun saveAudio(recognizedText: String, audioUri: Uri) {
-        Log.i(TAG, "save audio is called")
         var input: InputStream? = null
         var output: FileOutputStream? = null
         try {
@@ -235,9 +238,9 @@ class AudioRecordFragment : Fragment() {
 
             Util.getDuration(file)?.let {
                 withContext(Dispatchers.Main) {
-                    viewModel.addTimeAndText(recognizedText, it.toInt())
-                    dataBinding.tvAudioRecordShowRecognizedText.text =  viewModel.getAccText()
-                    viewModel.addFileToList(file)
+                    audioRecordViewModel.addTimeAndText(recognizedText, it.toInt())
+                    dataBinding.tvAudioRecordShowRecognizedText.text =  audioRecordViewModel.getAccText()
+                    audioRecordViewModel.addFileToList(file)
                 }
             }
         } catch (e: IOException) {
