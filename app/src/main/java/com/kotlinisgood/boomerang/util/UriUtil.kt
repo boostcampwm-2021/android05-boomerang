1b3f0f9922cd6dcebf49cf57c1630acbeff48965
greenddoovie <mania01234@naver.com>
2021-11-30 11:57:49 +0900

    refactor: Inspect code 반영하여 Lint 수정

--- app/src/main/java/com/kotlinisgood/boomerang/util/UriUtil.kt
+++ app/src/main/java/com/kotlinisgood/boomerang/util/UriUtil.kt
@@ -1,16 +1,14 @@
 package com.kotlinisgood.boomerang.util
 
-import android.annotation.SuppressLint
 import android.content.ContentResolver
 import android.net.Uri
 
 object UriUtil {
 
-    @SuppressLint("Range")
     fun getPathFromUri(contentResolver: ContentResolver, uri: Uri): String {
         val cursor = contentResolver.query(uri, null, null, null, null) ?: return ""
         cursor.moveToFirst()
-        val path = cursor.getString(cursor.getColumnIndex("_data"))
+        val path = cursor.getString(cursor.getColumnIndexOrThrow("_data"))
         cursor.close()
         return path
     }
